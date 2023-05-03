5550afa7f6d121b0a0cbcc17d9c8df5b4201fa45
Naipawat Poolsawat <tata-naipawat@hotmail.com>
2023-05-03 14:44:05 +0700

    Remove Unused Code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'dart:developer';
 import 'dart:math';
 
 import 'package:auto_size_text/auto_size_text.dart';
@@ -67,67 +66,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-//Stateless build once and never change
-//Does stateful widget build again when state change?
-//Stateful widget build again when state change
-class CountingPage extends StatelessWidget {
-  const CountingPage({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    // BlocProvider is a Flutter widget from the flutter_bloc package
-    // that provides a bloc to its children via BlocProvider.of<CounterCubit>(context)
-
-    return BlocProvider(
-      create: (_) => CounterCubit(),
-      child: const CountingView(),
-    );
-  }
-}
-
-class CountingView extends StatelessWidget {
-  const CountingView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Counting')),
-      // BlocBuilder is a widget that listens to state changes in the bloc
-      // and rebuilds the UI with the new state
-      body: BlocBuilder<CounterCubit, CounterState>(
-        builder: (context, state) {
-          return Center(
-            child: Text(
-              '${state.count}',
-              style: const TextStyle(fontSize: 24),
-            ),
-          );
-        },
-      ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            child: const Icon(Icons.add),
-            // The context.read method can be used to access the bloc
-            // and call methods on it
-            onPressed: () => context.read<CounterCubit>().changeCount(),
-          ),
-          const SizedBox(height: 8),
-          FloatingActionButton(
-            child: const Icon(Icons.remove),
-            onPressed: () => context.read<CounterCubit>().changeCount(),
-          ),
-        ],
-      ),
-    );
-  }
-}
-// Stateful Widget vs BLoc Pattern
-// why use bloc pattern ? 1. state management 2. code reuse 3. testability 4. code organization
-// 5. asynchronous events 6. stream of states 7. single source of truth
-// 8. separation of concerns 9. debugging 10. hot reload
-// 11. performance 12. scalability 13. simplicity 14. predictability
